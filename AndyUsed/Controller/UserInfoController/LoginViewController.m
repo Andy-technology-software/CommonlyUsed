9978e56f433355c6d24d88a9114fe9166b8707cb
Andy-technology-software <xu_renqiang@outlook.com>
2017-04-28 11:27:54 +0800

    JVFloatLabeledTextField的使用，登录浮动框

--- AndyUsed/Controller/UserInfoController/LoginViewController.m
+++ AndyUsed/Controller/UserInfoController/LoginViewController.m
@@ -13,21 +13,12 @@
     UITableView* _tableView;
 }
 
-@property(nonatomic,retain)UIImageView* loginIV;
-@property(nonatomic,retain)UIImageView* leftNameIV1;
-@property(nonatomic,retain)UIImageView* leftPWIV1;
-@property(nonatomic,retain)UIImageView* nameImageView;
-@property(nonatomic,retain)UIImageView* PWImageView;
+@property(nonatomic,retain)JVFloatLabeledTextField *nameTF;
+@property(nonatomic,retain)JVFloatLabeledTextField *PWTF;
+@property(nonatomic,strong)UIView* lineView;
+@property(nonatomic,strong)UIView* lineView1;
 
-@property(nonatomic,retain)UITextField* nameTF;
-@property(nonatomic,retain)UITextField* PWTF;
-
-@property(nonatomic,retain)UILabel* zhucewangjiLable;
-
-@property(nonatomic,retain)UIButton* leftDengluBtn;
 @property(nonatomic,retain)UIButton* dengluBtn;
-@property(nonatomic,retain)UIButton* zhuceBtn;
-@property(nonatomic,retain)UIButton* wangjimimaBtn;
 
 @property (nonatomic,assign)CLLocationCoordinate2D dingwei;
 
@@ -90,84 +81,61 @@
     return cell;
 }
 - (void)makecellUI:(UITableViewCell*)cell{
-    self.loginIV = [[UIImageView alloc] init];
-    self.loginIV.image = [UIImage imageNamed:@"登录-logo"];
-    [cell addSubview:self.loginIV];
-    [self.loginIV mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo([MyController getScreenWidth] / 2 - 43);
-        make.top.mas_equalTo(65);
-        make.width.mas_offset(86);
-        make.height.mas_offset(135);
-    }];
-    
-    self.nameImageView = [[UIImageView alloc] init];
-    [cell addSubview:self.nameImageView];
-    self.nameImageView.image = [UIImage imageNamed:@"登录-框"];
-    
-    [self.nameImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(0);
-        make.right.mas_equalTo(0);
-        make.top.mas_equalTo(self.loginIV.mas_bottom).offset(53);
-        make.height.mas_offset(52);
-    }];
-    
-    self.leftNameIV1 = [[UIImageView alloc] init];
-    self.leftNameIV1.image = [UIImage imageNamed:@"登录-1"];
-    [cell addSubview:self.leftNameIV1];
-    [self.leftNameIV1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(18);
-        make.top.mas_equalTo(self.nameImageView.mas_top).offset(16);
-        make.width.mas_offset(18);
-        make.height.mas_offset(18);
-    }];
-    
-    self.nameTF = [MyController createTextFieldWithFrame:cell.frame placeholder:@"请输入用户名" passWord:NO leftImageView:nil rightImageView:nil Font:16];
+    self.nameTF = [[JVFloatLabeledTextField alloc] init];
+    self.nameTF.font = [UIFont systemFontOfSize:16];
+    self.nameTF.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.nameTF.attributedPlaceholder = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"手机号", @"")
+                                                                       attributes:@{NSForegroundColorAttributeName: [UIColor darkGrayColor]}];
+    self.nameTF.floatingLabelFont = [UIFont boldSystemFontOfSize:14];
+    self.nameTF.floatingLabelTextColor = [UIColor brownColor];
+    self.nameTF.translatesAutoresizingMaskIntoConstraints = NO;
+    self.nameTF.keyboardType = UIKeyboardTypeNumberPad;
     [cell addSubview:self.nameTF];
-    [self.nameTF mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(65);
-        make.right.mas_equalTo(0);
-        make.top.mas_equalTo(self.nameImageView);
-        make.height.mas_offset(52);
+    
+    [self.nameTF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(40);
+        make.top.mas_equalTo(65);
+        make.right.mas_equalTo(-40);
+        make.height.mas_offset(60);
     }];
     
-    self.PWImageView = [[UIImageView alloc] init];
-    [cell addSubview:self.PWImageView];
-    self.PWImageView.image = [UIImage imageNamed:@"登录-框"];
+    self.lineView = [[UIView alloc] init];
+    [cell addSubview:self.lineView];
+    self.lineView.backgroundColor = [MyController colorWithHexString:@"d8d8d8"];
     
-    [self.PWImageView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(0);
-        make.right.mas_equalTo(0);
-        make.top.mas_equalTo(self.nameImageView.mas_bottom).offset(2);
-        make.height.mas_offset(52);
+    [self.lineView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.nameTF);
+        make.right.mas_equalTo(self.nameTF);
+        make.top.mas_equalTo(self.nameTF.mas_bottom);
+        make.height.mas_offset(0.5);
     }];
     
-    self.leftPWIV1 = [[UIImageView alloc] init];
-    self.leftPWIV1.image = [UIImage imageNamed:@"登录-2"];
-    [cell addSubview:self.leftPWIV1];
-    [self.leftPWIV1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(18);
-        make.top.mas_equalTo(self.PWImageView.mas_top).offset(16);
-        make.width.mas_offset(18);
-        make.height.mas_offset(18);
-    }];
-    
-    self.PWTF = [MyController createTextFieldWithFrame:cell.frame placeholder:@"请输入密码" passWord:YES leftImageView:nil rightImageView:nil Font:16];
+    self.PWTF = [[JVFloatLabeledTextField alloc] init];
+    self.PWTF.font = [UIFont systemFontOfSize:16];
+    self.PWTF.attributedPlaceholder = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"密码", @"")
+                                                                        attributes:@{NSForegroundColorAttributeName: [UIColor darkGrayColor]}];
+    self.PWTF.floatingLabelFont = [UIFont boldSystemFontOfSize:14];
+    self.PWTF.floatingLabelTextColor = [UIColor brownColor];
+    self.PWTF.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.PWTF setSecureTextEntry:YES];
     [cell addSubview:self.PWTF];
-    [self.PWTF mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(65);
-        make.right.mas_equalTo(0);
-        make.top.mas_equalTo(self.PWImageView);
-        make.height.mas_offset(52);
+    
+    [self.PWTF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.nameTF);
+        make.top.mas_equalTo(self.lineView.mas_bottom).mas_offset(40);
+        make.right.mas_equalTo(self.nameTF);
+        make.height.mas_equalTo(self.nameTF);
     }];
     
-    self.leftDengluBtn = [MyController createButtonWithFrame:cell.frame ImageName:@"登录-箭头" Target:self Action:@selector(leftDengluBtnClick) Title:nil];
-    [cell addSubview:self.leftDengluBtn];
+    self.lineView1 = [[UIView alloc] init];
+    [cell addSubview:self.lineView1];
+    self.lineView1.backgroundColor = [MyController colorWithHexString:@"d8d8d8"];
     
-    [self.leftDengluBtn mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(0);
-        make.top.mas_equalTo(self.PWImageView.mas_bottom).offset(10);
-        make.height.mas_offset(50);
-        make.width.mas_offset(52);
+    [self.lineView1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(self.PWTF);
+        make.right.mas_equalTo(self.PWTF);
+        make.top.mas_equalTo(self.PWTF.mas_bottom);
+        make.height.mas_offset(0.5);
     }];
     
     self.dengluBtn = [MyController createButtonWithFrame:cell.frame ImageName:nil Target:self Action:@selector(dengluBtnClick) Title:@"登录"];
@@ -177,45 +145,11 @@
     [cell addSubview:self.dengluBtn];
     
     [self.dengluBtn mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.leftDengluBtn.mas_right);
-        make.top.mas_equalTo(self.leftDengluBtn);
-        make.right.mas_equalTo(0);
-        make.height.mas_equalTo(self.leftDengluBtn);
+        make.left.mas_equalTo(self.PWTF);
+        make.top.mas_equalTo(self.lineView1.mas_bottom).mas_offset(40);
+        make.right.mas_equalTo(self.PWTF);
+        make.height.mas_offset(40);
     }];
-    
-    self.zhucewangjiLable = [[UILabel alloc] init];
-    self.zhucewangjiLable.text = @"立即注册  | 忘记密码 ？";
-    [cell addSubview:self.zhucewangjiLable];
-    self.zhucewangjiLable.textColor = [UIColor whiteColor];
-    self.zhucewangjiLable.numberOfLines = 1;
-    self.zhucewangjiLable.font = [UIFont systemFontOfSize:12];
-    
-    [self.zhucewangjiLable mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.mas_equalTo(-5);
-        make.top.mas_equalTo(self.dengluBtn.mas_bottom).offset(13);
-        make.height.mas_offset(12);
-    }];
-    
-    self.zhuceBtn = [MyController createButtonWithFrame:cell.frame ImageName:nil Target:self Action:@selector(zhuceBtnClick) Title:nil];
-    [cell addSubview:self.zhuceBtn];
-    
-    [self.zhuceBtn mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(200);
-        make.top.mas_equalTo(self.dengluBtn.mas_bottom);
-        make.right.mas_equalTo(-65);
-        make.height.mas_offset(30);
-    }];
-    
-    self.wangjimimaBtn = [MyController createButtonWithFrame:cell.frame ImageName:nil Target:self Action:@selector(wangjimimaBtnClick) Title:nil];
-    [cell addSubview:self.wangjimimaBtn];
-    
-    [self.wangjimimaBtn mas_updateConstraints:^(MASConstraintMaker *make) {
-        make.left.mas_equalTo(self.zhuceBtn.mas_right);
-        make.top.mas_equalTo(self.zhuceBtn);
-        make.right.mas_equalTo(0);
-        make.height.mas_equalTo(self.zhuceBtn);
-    }];
-    
 }
 #pragma mark - 左边登录响应
 - (void)leftDengluBtnClick{
@@ -279,16 +213,14 @@
         [JPUSHService setAlias:model.userName callbackSelector:@selector(callBack) object:nil];
         
         //登录环信
-//        [[EMClient sharedClient] loginWithUsername:self.nameTF.text password:self.nameTF.text completion:^(NSString *aUsername, EMError *aError) {
-//            if (!aError) {
-//                NSLog(@"登陆成功");
-//                [[EMClient sharedClient] setApnsNickname:model.nickName];
-//            } else {
-//                NSLog(@"登陆失败");
-//            }
-//        }];
-        
-        [[EMClient sharedClient] loginWithUsername:self.nameTF.text password:self.nameTF.text];
+        [[EMClient sharedClient] loginWithUsername:self.nameTF.text password:self.nameTF.text completion:^(NSString *aUsername, EMError *aError) {
+            if (!aError) {
+                NSLog(@"登陆成功");
+                [[EMClient sharedClient] setApnsNickname:model.nickName];
+            } else {
+                NSLog(@"登陆失败");
+            }
+        }];
         
         [(AppDelegate *)[UIApplication sharedApplication].delegate setRootVC];
     } failure:^(NSError *error) {
